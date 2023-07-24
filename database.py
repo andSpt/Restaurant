7294d1c1b17f7cd55401fa91122645811060b20d
Andrey Sipatov <Henrrry52@gmail.com>
2023-07-24 10:16:57 +0400

    Added  requirements.txt

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+
 from config import load_config
 
 
