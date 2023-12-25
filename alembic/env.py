ccd8b91e9acbe14efbef0d18078d097377488091
Andrey Sipatov <118002105+andSpt@users.noreply.github.com>
2023-12-25 21:12:34 +0300

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -4,8 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 
-from database import Base
-from config import load_config
+from app.core.database import Base
+from app.config import load_config
 
 
 conf = load_config('../.env')
